959ea7434a59938bf82eb91de3aec42084df78a6
Jor_Poon <jormpoon@gmail.com>
2019-03-18 16:39:35 -0400

    able to get users through .find

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json())
+
+server.get('/users', (req, res) => {
+    db
+      .find('users')
+      .then(users => {
+          res.status(200).json(users)
+      })  
+      .catch(error => {
+          res.status(500).json({error: "The users information could not be retrieved."})
+      });
+})
+
+
+
+
+
+
+
+
+
+server.listen(4000, () => {
+    console.log('\n** API up and running on port 4k **')
+})
